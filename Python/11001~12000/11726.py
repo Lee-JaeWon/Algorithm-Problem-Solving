35a5bb632e27b45202a93b64f528367c906146f0
Lee-JaeWon <jawwoni@naver.com>
2021-09-05 23:17:03 +0900

    BOJ 11726

---
+++ Python/11001~12000/11726.py
@@ -0,0 +1,12 @@
+import sys
+input =  sys.stdin.readline
+
+n = int(input())
+
+dp = [0, 1, 2]
+if n > 2:
+    for i in range(3, n+1):
+        dp.append(dp[i-1]+dp[i-2])
+print(dp[n] % 10007)
+        
+
